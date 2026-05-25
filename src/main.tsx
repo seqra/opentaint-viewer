580381ae104dfa4019a40a2e0ec3554735d03ff7
Aleksandr Misonizhnik <misonijnik@gmail.com>
2026-05-25 18:42:42 +0300

    feat: bundle JetBrains Mono webfont

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import '@fontsource/jetbrains-mono/400.css';
+import '@fontsource/jetbrains-mono/600.css';
+import '@fontsource/jetbrains-mono/700.css';
 import './theme.css';
 
 createRoot(document.getElementById('root')!).render(
